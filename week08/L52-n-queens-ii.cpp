54b766991770f87ee757dbbf95e2ab673fb99c9e
shouhong <shouhong.zhang@outlook.com>
2020-12-13 22:10:21 +0800

    L52-N皇后II

--- week08/L52-n-queens-ii.cpp
+++ week08/L52-n-queens-ii.cpp
@@ -9,11 +9,6 @@
 //   (2) x & (x - 1) 代表将最后一位 1 变成 0
 // 4. 数的存储用补码：正数的补码为自身，负数的补码为其反码+1
 
-作者：makeex
-链接：https://leetcode-cn.com/problems/n-queens-ii/solution/dfs-wei-yun-suan-jian-zhi-by-makeex/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
-
 #include <iostream>
 using namespace std;
 
